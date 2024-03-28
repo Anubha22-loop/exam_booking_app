429878a08e6722109b6f4303c50307dc00ef2a84
Anubha Gupta <anubha.gupta@finaxar.com>
2024-03-28 12:56:38 +0530

    Move controller and model validation and success messages to localization file

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -19,12 +19,12 @@ class Api::UsersController < ApplicationController
         exam_start_time: create_params[:start_time].to_datetime
       )
       if api_request.save
-        render json: { message: "User created successfully" }, status: :ok
+        render json: { message: I18n.t('controller.api.users.create.success') }, status: :ok
       else
-        render json: { error: "Failed to associate User with the Exam" }, status: :bad_request
+        render json: { error: I18n.t('controller.api.users.create.api_request_creation_failure') }, status: :bad_request
       end
     else
-      render json: { error: "Failed to create user" }, status: :bad_request
+      render json: { error: I18n.t('controller.api.users.create.user_creation_failure') }, status: :bad_request
     end
     
   end
@@ -49,22 +49,22 @@ class Api::UsersController < ApplicationController
   end
 
   def missing_param_error
-    render json: { error: "Required parameter are missing" }, status: :bad_request
+    render json: { error: I18n.t('controller.api.users.create.paramete_missing_error') }, status: :bad_request
   end
 
   def validate_college
     college = College.find_by(id: create_params[:college_id])
-    render json: { error: "College with the given ID not found" }, status: :bad_request unless college
+    render json: { error: I18n.t('controller.api.users.create.invalid_college_failure') }, status: :bad_request unless college
   end
 
   def validate_exam
     exam = Exam.find_by(id: create_params[:exam_id])
-    render json: { error: "Exam with the given ID not found or does not belong to the College" }, status: :bad_request unless exam && exam.college_id == create_params[:college_id].to_i
+    render json: { error: I18n.t('controller.api.users.create.invalid_exam_failure') }, status: :bad_request unless exam && exam.college_id == create_params[:college_id].to_i
   end
 
   def validate_exam_window
-    exam_window = Exam.find_by(id: create_params[:exam_id]).exam_window
-    exam_time_range = exam_window.start_time..exam_window.end_time
-    render json: { error: "Requested start time does not fall within the Exam's time window" }, status: :bad_request unless exam_time_range.cover?(create_params[:start_time].to_datetime)
+    exam_window = Exam.find_by(id: create_params[:exam_id])&.exam_window
+    exam_time_range = exam_window&.start_time..exam_window&.end_time
+    render json: { error: I18n.t('controller.api.users.create.invalid_exam_time_failure')}, status: :bad_request unless exam_time_range.cover?(create_params[:start_time].to_datetime)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
-  validates :phone_no, length: { is: 10, message: "should be 10 digits long" }
+  validates :phone_no, length: { is: 10, message: I18n.t('model.user.errors.invalid_phone_number_length') }
   validates :phone_no, uniqueness: true
-  validates :phone_no, presence: true, format: { with: /\A\d+\z/, message: "should contain only numeric characters" }
+  validates :phone_no, presence: true, format: { with: /\A\d+\z/, message: I18n.t('model.user.errors.invalid_phone_number') }
 
   has_many :api_requests
 end
