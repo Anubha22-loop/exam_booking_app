4d75f64e4c83e9446036e3c9124c675c2b834a42
Anubha Gupta <anubha.gupta@finaxar.com>
2024-03-28 20:03:59 +0530

    added data type sanity check for exams controller

--- app/controllers/api/exams_controller.rb
+++ app/controllers/api/exams_controller.rb
@@ -1,6 +1,6 @@
 class Api::ExamsController < ApplicationController
   rescue_from ActionController::ParameterMissing, with: :missing_param_error
-  before_action :validate_college
+  before_action :validate_params_type, :validate_college
 
   def create
     exam = Exam.new(
@@ -27,12 +27,18 @@ class Api::ExamsController < ApplicationController
     end
   end
 
+  def validate_params_type
+    unless create_params[:exam_name].is_a?(String) && create_params[:college_id].is_a?(Integer)
+      render json: { error: I18n.t('controller.api.exams.create.invalid_data_type') }, status: :bad_request
+    end
+  end
+
   def validate_college
     college = College.find_by(id: create_params[:college_id])
     render json: { error: I18n.t('controller.api.exams.create.invalid_college_failure') }, status: :bad_request unless college
   end
 
   def missing_param_error
-    render json: { error: I18n.t('controller.api.exams.create.paramete_missing_error') }, status: :bad_request
+    render json: { error: I18n.t('controller.api.exams.create.parameter_missing_error') }, status: :bad_request
   end
 end
--- spec/controllers/api/exams_controller_spec.rb
+++ spec/controllers/api/exams_controller_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe Api::ExamsController, type: :controller do
       }
 
       it 'should give status code 200 and successful message' do
-        post :create, params: request
+        post :create, params: request, as: :json
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)['message']).to eq("Exam created successfully")
       end
 
       it 'should increase the count of exam by one' do
-        expect { post :create, params: request }.to change{ Exam.count }.by(1)
+        expect { post :create, params: request, as: :json }.to change{ Exam.count }.by(1)
         exam = Exam.last
         expect(exam.name).to eq(request[:exam_name])
       end
@@ -27,31 +27,40 @@ RSpec.describe Api::ExamsController, type: :controller do
     context 'with invalid request' do
       it 'gives bad request if one of the parameters are missing' do
         request = { :exam_name => 'test' }
-        post :create, params: request
+        post :create, params: request, as: :json
         expect(response).to have_http_status(:bad_request)
         expect(JSON.parse(response.body)['error']).to eq("Required parameter are missing")
       end
 
       it 'gives bad request if college doesnot exist for the given college_id in api request' do
-        request = 
-          {
+        request = {
             :exam_name => 'test1',
             :college_id => 4
           }
-          post :create, params: request
+        post :create, params: request, as: :json
         expect(response).to have_http_status(:bad_request)
         expect(JSON.parse(response.body)['error']).to eq("College with the given ID not found")
+      end
+
+      it 'gives bad request if parameter datatype is incorrect' do
+        college = create(:college)
+        request = {
+            :exam_name => 1,
+            :college_id => college.id
+          }
+        post :create, params: request, as: :json
+        expect(response).to have_http_status(:bad_request)
+        expect(JSON.parse(response.body)['error']).to eq("Invalid data types for one or more params")
       end
 
       it 'gives bad request if user creation fails' do
         college = create(:college)
         allow_any_instance_of(Exam).to receive(:save).and_return(false)
-        request = 
-          {
+        request = {
             :exam_name => 'test1',
             :college_id => college.id,
           }
-          post :create, params: request
+        post :create, params: request, as: :json
         expect(response).to have_http_status(:bad_request)
         expect(JSON.parse(response.body)['error']).to eq("Failed to create exam")
       end
